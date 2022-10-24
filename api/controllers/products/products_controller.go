026ef7223a1c8c7d4fa4bb806c1b3b407fc109ec
konno <konno@DESKTOP-CBC032F.localdomain>
2022-10-24 18:39:40 +0900

    implement controller

--- api/controllers/products/products_controller.go
+++ api/controllers/products/products_controller.go
@@ -3,29 +3,46 @@
 package products
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gouser/api/domain/products"
+	"github.com/gouser/api/services"
+	"github.com/gouser/api/utils/errors"
 )
 
 // CreateProduct - Create product
 func CreateProduct(c *gin.Context) {
 	var product products.Product
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		log.Println(err.Error())
+	if err := c.ShouldBindJSON(&product); err != nil {
+		apiErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(apiErr.Status, apiErr)
 		return
 	}
-	if err := json.Unmarshal(bytes, &product); err != nil {
-		log.Println(err.Error())
+	newProduct, saveErr := services.CreateProduct(product)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
 		return
 	}
-	fmt.Println(string(bytes))
-	fmt.Println(err)
-	c.JSON(http.StatusOK, product)
+	c.JSON(http.StatusCreated, newProduct)
 }
+
+// GetProduct - Get product by product id
+func GetProduct(c *gin.Context) {
+	productID, productErr := strconv.ParseUint(c.Param("product_id"), 10, 64)
+	if productErr != nil {
+		err := errors.NewBadRequestError("product id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+ 
+	product, getErr := services.GetProduct(productID)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, product)
+ 
+}
+ 
---
+++ api/utils/errors/api_errors.go
@@ -0,0 +1,27 @@
+// api_errors.go
+
+package errors
+
+import "net/http"
+
+type ApiErr struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+	Error   string `json:"error"`
+}
+
+func NewBadRequestError(message string) *ApiErr {
+	return &ApiErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+func NewNotFoundError(message string) *ApiErr {
+	return &ApiErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
